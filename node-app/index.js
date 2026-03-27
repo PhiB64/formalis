b6ddc76528469cf4bc6cfaf7565f17683b7e3adc
Philippe <philippebarbosa64@gmail.com>
2026-03-27 12:02:42 +0100

    Mise en place de la configuration Docker

---
+++ node-app/index.js
@@ -0,0 +1,15 @@
+
+import 'dotenv/config';
+import express from 'express';
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello');
+});
+
+app.listen(port, () => {
+  console.log(`Express server running on port ${port}`);
+});
